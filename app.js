4afc73686035e6ad13f2b2ee0c36b508d209fd13
nikhil_kumar <nkman2002@gmail.com>
2024-08-11 18:58:38 +0530

    search fun

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
-const url = "http://api.weatherapi.com/v1/current.json?key=a3c4f08b23834fd3b55174848241008&q=Indias&aqi=yes"
-const inp = document.querySelector(".weather")
-const search_btn = document.querySelector(".search-btn")
-const wdata = document.querySelector(".weather-data")
 
 
-const  getdata =  async function () {
-   await fetch(url)
-  .then((res) => {
-    return res.json();
+const btn = document.querySelector(".search-btn");
+const inp = document.querySelector("#weather")
+const wdata = document.querySelector(".wdata")
+const loc_btn = document.querySelector('.location_btn')
+
+
+ async function checkweather (city){
+  const api_key = `27eb7d60a861c28ca6c16561d382b115`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+ await fetch(`${url}`)
+  .then((res) =>{
+    return res.json()
   })
-  .then((data) => {
+  .then((data)=>{
     console.log(data);
-      wdata.innerHTML  = `              
-         <h5>Temprature_c: ${data.current.temp_c}</h5>
-         <h5>wind_kph: ${data.current.wind_kph}</h5>
-         <h5>Humidity: ${data.current.humidity}</h4>
-       
-         `
-
-         document.querySelector(".weather-img").innerHTML = `
-         <img src= ${ '//cdn.weatherapi.com/weather/64x64/night/353.png'} alt="">
-         <h4>${data.current.condition.text}</h4>
-         `
+        wdata.innerHTML = `<div class="weather-img">
+                
+            </div>
+            <div class="weather-data">
+               <h2>temprature: ${Math.floor(data.main.temp - 273.15)}°C</h2>
+               <h2>wind:${data.wind.speed}kmphs</h2>
+                <h2>Humidity:${data.main.humidity}%</h2>`
   })
-  
 }
 
-search_btn.addEventListener("click", getdata)
+
+btn.addEventListener("click",  ()=>{
+   checkweather(inp.value)
+})
+
+//  async function getlocation(lat ,long){
+//   const api_key = `27eb7d60a861c28ca6c16561d382b115`;
+//     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${lat},${long}&appid=${api_key}`)
+//     .then((res) =>  {return  res.json()})
+//     .then((data) => {
+//       console.log(data);
+       
+//     })
+// }
+
+
+
+
+
+// async function getres (positon){
+//    console.log(positon);
+//    const result =  await getlocation(positon.coords.latitude)
+//    console.log(result);
+     
+// }
+// const geterr = function(){
+//   console.log("some error");
+  
+// }
+
+// loc_btn.addEventListener("click", ()=> {
+//   navigator.geolocation.getCurrentPosition(getres , geterr)
+// })
+
